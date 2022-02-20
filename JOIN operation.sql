75b857e52b165e29be09ed74e51dfa09d7ffc9fb
avantika tellawar <68954508+avantikat@users.noreply.github.com>
2022-02-20 14:48:07 +0530

    Update JOIN operation.sql

--- JOIN operation.sql
+++ JOIN operation.sql
@@ -14,17 +14,14 @@ Notice in the that the column matchid in the goal table corresponds to the id co
 
 Show id, stadium, team1, team2 for just game 1012
 
-SELECT id,stadium,team1,team2
+ 
+ SELECT id,stadium,team1,team2
   FROM game
 where id = 1012
 
 3.
 You can combine the two steps into a single query with a JOIN.
 
-SELECT *
-  FROM game JOIN goal ON (id=matchid)
-The FROM clause says to merge data from the goal table with that from the game table. The ON says how to figure out which rows in game go with which rows in goal - the matchid from goal must match id from game. (If we wanted to be more clear/specific we could say
-ON (game.id=goal.matchid)
 
 The code below shows the player (from the goal) and stadium name (from the game table) for every goal scored.
 
@@ -34,6 +31,12 @@ SELECT player,teamid,stadium,mdate
   FROM game JOIN goal ON (id=matchid)
  WHERE teamid='GER'
  
+ 
+ 4.Use the same JOIN as in the previous question.
+
+Show the team1, team2 and player for every goal scored by a player called Mario player LIKE 'Mario%'
+
+
  SELECT team1, team2, player
   FROM game JOIN goal ON (id=matchid)
 WHERE player LIKE 'Mario%';
